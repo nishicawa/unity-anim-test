b2fe0904b8065fac400324c17724b5c0441871a0
nishicawa <crevice@refuse.jp>
2018-02-08 01:50:16 +0900

    first commit(true)

---
+++ Assets/RandomStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomStart : StateMachineBehaviour
+{
+    private int wait_time = 0;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        wait_time = Random.Range(0, 60 * 5);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (wait_time == 0)
+            animator.SetTrigger("MoveTrigger");
+        else
+            wait_time--;
+        Debug.Log(wait_time);
+    }
+}
